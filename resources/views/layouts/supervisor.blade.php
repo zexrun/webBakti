e69aa7f2d157e3db0cb90cf559bc715a75d0b378
rifqykhuzaini <rifqykhuzaini@gmail.com>
2025-07-17 15:11:16 +0700

    fix: Supervisor Nav Adjustment

--- resources/views/layouts/supervisor.blade.php
+++ resources/views/layouts/supervisor.blade.php
@@ -25,28 +25,38 @@
             </div>
 
             <!-- 🔔 Notifikasi -->
-            <div x-data="{ open: false }" class="relative">
-                <button @click="open = !open" class="relative text-white focus:outline-none">
-                    🔔
-                    @if($unreadNotifications->count() > 0)
-                        <span class="absolute -top-1 -right-1 bg-red-600 text-white text-xs px-1.5 py-0.5 rounded-full">
-                            {{ $unreadNotifications->count() }}
-                        </span>
-                    @endif
-                </button>
+                        <!-- Kanan: Notifikasi & Logout -->
+            <div class="flex items-center space-x-4">
+                <!-- 🔔 Notifikasi -->
+                <div x-data="{ open: false }" class="relative">
+                    <button @click="open = !open" class="relative text-white focus:outline-none">
+                        🔔
+                        @if($unreadNotifications->count() > 0)
+                            <span class="absolute -top-1 -right-1 bg-red-600 text-white text-xs px-1.5 py-0.5 rounded-full">
+                                {{ $unreadNotifications->count() }}
+                            </span>
+                        @endif
+                    </button>
 
-                <!-- Dropdown -->
-                <div x-show="open" @click.away="open = false" class="absolute right-0 mt-2 w-80 bg-white text-black rounded shadow-lg z-50">
-                    @forelse ($unreadNotifications as $notification)
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 border-b text-sm">
-                            {{ $notification->data['message'] }}
-                        </a>
-                    @empty
-                        <div class="px-4 py-2 text-sm text-gray-500">
-                            Tidak ada notifikasi baru.
-                        </div>
-                    @endforelse
+                    <!-- Dropdown -->
+                    <div x-show="open" @click.away="open = false" class="absolute right-0 mt-2 w-80 bg-white text-black rounded shadow-lg z-50">
+                        @forelse ($unreadNotifications as $notification)
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 border-b text-sm">
+                                {{ $notification->data['message'] }}
+                            </a>
+                        @empty
+                            <div class="px-4 py-2 text-sm text-gray-500">
+                                Tidak ada notifikasi baru.
+                            </div>
+                        @endforelse
+                    </div>
                 </div>
+
+                <!-- Logout -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="hover:text-red-700">Logout</button>
+                </form>
             </div>
         </div>
     </nav>
