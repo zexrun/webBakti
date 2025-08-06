47aa7020bd07aa65e7e2b5074f01587cddeb2a8c
rifqykhuzaini <rifqykhuzaini@gmail.com>
2025-08-06 15:03:35 +0700

    fix: Minor Fix on Migrations

--- database/migrations/2025_07_09_034450_create_tasks_table.php
+++ database/migrations/2025_07_09_034450_create_tasks_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->text('description');
             $table->string('file_path')->nullable();
             $table->enum('type', ['harian', 'akhir']);
-            $table->datetimes('due_date')->nullable();
+            $table->datetime('due_date')->nullable();
             $table->timestamps();
         });
     }
