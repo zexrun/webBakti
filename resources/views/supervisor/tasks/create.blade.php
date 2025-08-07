539102d12843ac06135dfdf587b1734a66dd50b1
rifqykhuzaini <rifqykhuzaini@gmail.com>
2025-08-07 14:28:40 +0700

    fix: Update Supervisor UI

--- resources/views/supervisor/tasks/create.blade.php
+++ resources/views/supervisor/tasks/create.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="min-h-screen bg-gray-50 py-8">
-    <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <!-- Header -->
         <div class="mb-8">
             <div class="flex items-center mb-4">
