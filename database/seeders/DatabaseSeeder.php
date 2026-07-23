089ce9ba4eddc1d0fcc9063c4cb83d7b73855d80
rifqykhuzaini <rifqykhuzaini@gmail.com>
2026-07-23 18:34:46 +0700

    feat: Register all new seeders in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,23 @@
 
 namespace Database\Seeders;
 
-use Database\Seeders\AdminSeeder; // Menggunakan namespace yang benar
-use Database\Seeders\DirectorateSeeder; // Menggunakan namespace yang benar
-use Database\Seeders\PositionSeeder; // Menggunakan namespace yang benar
-use Database\Seeders\SupervisorSeeder; // Menggunakan namespace yang benar
-use Database\Seeders\StudentSeeder; 
-use Database\Seeders\TaskSeeder; 
-use Database\Seeders\UniversitySeeder; 
+use Database\Seeders\AdminSeeder;
+use Database\Seeders\DirectorateSeeder;
+use Database\Seeders\PositionSeeder;
+use Database\Seeders\SupervisorSeeder;
+use Database\Seeders\StudentSeeder;
+use Database\Seeders\StudentSupervisorAssignmentSeeder;
+use Database\Seeders\TaskSeeder;
+use Database\Seeders\UniversitySeeder;
+use Database\Seeders\AttendanceSettingSeeder;
+use Database\Seeders\AttendanceSeeder;
+use Database\Seeders\AttendanceExceptionSeeder;
+use Database\Seeders\SubmissionSeeder;
+use Database\Seeders\FinalAssessmentSeeder;
+use Database\Seeders\DocumentSeeder;
+use Database\Seeders\LogbookSeeder;
+use Database\Seeders\MessageSeeder;
+use Database\Seeders\AnnouncementSeeder;
 
 use Illuminate\Database\Seeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -26,7 +36,18 @@ class DatabaseSeeder extends Seeder
             PositionSeeder::class,
             SupervisorSeeder::class,
             StudentSeeder::class,
+            StudentSupervisorAssignmentSeeder::class,
             UniversitySeeder::class,
+            AttendanceSettingSeeder::class,
+            AttendanceSeeder::class,
+            AttendanceExceptionSeeder::class,
+            TaskSeeder::class,
+            SubmissionSeeder::class,
+            FinalAssessmentSeeder::class,
+            DocumentSeeder::class,
+            LogbookSeeder::class,
+            MessageSeeder::class,
+            AnnouncementSeeder::class,
         ]);
     }
 }
